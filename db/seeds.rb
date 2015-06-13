e51d810418218a14458af0b436a54771640cb128
Go Sato <i97506051502@gmail.com>
2015-06-13 20:03:27 +0900

    Heroku を使うために色々 bundle install した

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,31 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Item.create(
-  name: "エアマックス 95",
-  description: "1995年のランニングマックスモデルの復刻版。時代を席巻した名モデルAIR MAX 95。当時は斬新でハイテクなルックスがストリートのファッションシーンを席巻しました。",
-  price: 25000,
-  seller_id: 746,
-  email: "daisuke@nashiro.org",
-  image_url: "http://news.digitallab.biz/wordpress/wp-content/uploads/Nike-Air-Max-95-Neon.jpg"
+Seller.create(
+  name: "佐藤豪",
+  prefecture: "東京都",
+  tel: "012-345-6789"
 )
 
-Item.create(
-  name: "フットスケープ",
-  description: "横にシューレースがある斬新なモデル。大切にしていただける方にお譲りします。",
-  price: 18000,
-  seller_id: 32,
-  email: "foo@example.com",
-  image_url: "http://www.furugikaitori.co.jp/auction/item/F351-1.jpg"
+Seller.create(
+  name: "佐藤隆",
+  prefecture: "長崎県",
+  tel: "095-820-9988"
 )
-
-item3 = Item.new(
-  name: "ポンプフューリー",
-  description: "ポンプを押すと、空気によってパイプが膨らみ足型にフィットします。",
-  price: 18000,
-  seller_id: 32,
-  email: "foo@example.com",
-  image_url: "http://image.rakuten.co.jp/atmos-girls/cabinet/reebok/v47514-1.jpg"
-)
-
-item3.save
